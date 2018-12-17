542f03a25e5b963700dd631b17325d846056d6f8
jey <geetan123321@gmail.com>
2018-12-17 15:02:32 +0530

    more commits

--- lib/time_measure.rb
+++ lib/time_measure.rb
@@ -1,3 +1,18 @@
 module TimeMeasure
+  def perf_init
+    @perf = Time.now
+  end
 
+  def perf_measure(logger = nil)
+    if logger
+      logger.info "$" * 10
+      logger.info caller_locations[0]
+      logger.info Time.now - @perf
+    else
+      puts "$" * 10
+      puts caller_locations[0]
+      puts Time.now - @perf
+    end
+    @perf = Time.now
+  end
 end
